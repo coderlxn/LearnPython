8c0e6cdda0eb67902ccf58b6bd5d297c8a1cfc4c
lixiaoning <lixn@dogesoft.cn>
2018-07-11 19:51:41 +0800

    表单验证

--- mega/app/views.py
+++ mega/app/views.py
@@ -1,7 +1,24 @@
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World! Again"
+    user = {"nickname": "Miguel"}
+    posts = [
+    {
+    	"author": {"nickname": "John"},
+    	"body": "Beautiful day in Portland!"
+    },
+    {
+    	"author": {"nickname": "Susan"},
+    	"body": "The Avengers movie was so cool!"
+    }]
+    return render_template("index.html", title="Home", user=user, posts=posts)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign In", form=form)
