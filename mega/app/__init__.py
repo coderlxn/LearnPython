f95e271de9dc2f505af73b85bbc52b1b69dd42e2
Vector <anyetiangong@qq.com>
2018-07-20 20:24:06 +0800

    after login

--- mega/app/__init__.py
+++ mega/app/__init__.py
@@ -7,10 +7,12 @@ from config import basedir
 
 app = Flask(__name__)
 app.config.from_object("config")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.init_app(app)
+lm.login_view = "login"
 oid = OpenID(app, os.path.join(basedir, "tmp"))
 
 from app import views, models
--- mega/app/views.py
+++ mega/app/views.py
@@ -12,8 +12,10 @@ def load_user(id):
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
-    user = {"nickname": "Miguel"}
+    return "Hello Flask!"
+    user = g.user
     posts = [
         {
             "author": {"nickname": "John"},
@@ -36,3 +38,29 @@ def login():
         session["remember_me"] = form.remember_me.data
         return oid.try_login(form.openid.data, ask_for=["nickname", "email"])
     return render_template("login.html", title="Sign In", form=form, providers=app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash("Invalid login. Please try again.")
+        return redirect(url_for("login"))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split("@")[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if "remember_me" in session:
+        remember_me = session["remember_me"]
+        session.pop("remember_me", None)
+    logout_user(user, remember=remember_me)
+    return redirect(request.args.get("next") or url_for("index"))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
