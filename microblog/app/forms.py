f1d841bf0881717b32fe6a0995d17611fcea2c69
Vector <anyetiangong@qq.com>
2018-08-20 19:56:38 +0800

    发布新内容

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -43,3 +43,8 @@ class EditorProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError("Please input a different username.")
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField("Say something", validators=[DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField("Submit")
--- microblog/microblog.py
+++ microblog/microblog.py
@@ -7,4 +7,4 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Post':Post}
 
 
-app.run(host="0.0.0.0", debug=False, port=5000)
+app.run(host="0.0.0.0", debug=True, port=5000)
