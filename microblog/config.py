53b90eaa64e57f9011270962f5596e2cd02f23a8
Vector <anyetiangong@qq.com>
2018-08-23 19:49:00 +0800

    数据分页显示

--- microblog/config.py
+++ microblog/config.py
@@ -13,3 +13,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ['anyetiangong@qq.com']
+
+    POSTS_PER_PAGE = 3
+
