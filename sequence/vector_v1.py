8b0a51fe6bb03cd205b8975836319bbe8f19979a
Vector <anyetiangong@qq.com>
2018-12-14 19:43:58 +0800

    yield send

--- sequence/vector_v1.py
+++ sequence/vector_v1.py
@@ -8,6 +8,7 @@
 from array import array
 import reprlib
 import math
+import numbers
 
 
 class Vector:
@@ -39,8 +40,61 @@ class Vector:
     def __bool__(self):
         return bool(abs(self))
 
+    def __len__(self):
+        return len(self._components)
+
+    def __getitem__(self, index):
+        cls = type(self)
+        if isinstance(index, slice):
+            return cls(self._components[index])
+        elif isinstance(index, numbers.Integral):
+            return self._components[index]
+        else:
+            msg = "{cls.__name__} indices must be integers"
+            raise TypeError(msg.format(cls=cls))
+
     @classmethod
     def frombytes(cls, octets):
         typecode = chr(octets[0])
         memv = memoryview(octets[1:]).cast(typecode)
         return cls(memv)
+
+    shortcut_names = 'xyzt'
+
+    def __getattr__(self, name):
+        cls = type(self)
+
+        if len(name) == 1:
+            pos = cls.shortcut_names.find(name)
+            if 0 <= pos < len(self._components):
+                return self._components[pos]
+        msg = "{.__name__!r} object has no attribute {!r}"
+        raise AttributeError(msg.format(cls, name))
+
+    def __setattr__(self, name, value):
+        cls = type(self)
+        if len(name) == 1:
+            if name in cls.shortcut_names:
+                error = "readonly attribute {attr_name!r}"
+            elif name.islower():
+                error = "can't set attributes 'a' to 'z' in {cls_name!r}"
+            else:
+                error = ""
+            if error:
+                msg = error.format(cls_name=cls.__name__, attr_name=name)
+                raise AttributeError(msg)
+        super().__setattr__(name, value)
+
+
+if __name__ == '__main__':
+    v7 = Vector(range(7))
+    print(v7[-1])
+    print(v7[1:4])
+    # print(v7[1, 2])
+
+    v = Vector(range(5))
+    print(v)
+    print(v.x)
+    v.x = 10  # 本应该是只读的
+    print(v.x)
+    print(v)
